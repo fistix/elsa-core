55593c1e233c400951548a537c229246bf96026e
Muhammad Bilal <muhammad.bilal@fistix.com>
2020-10-26 11:10:36 +0500

    test

--- src/core/Elsa.Abstractions/Design/ActivityPropertyTypes.cs
+++ src/core/Elsa.Abstractions/Design/ActivityPropertyTypes.cs
@@ -7,5 +7,6 @@ namespace Elsa.Design
         public const string Expression = "expression";
         public const string List = "list";
         public const string Select = "select";
+        //test
     }
 }
